63b6960cf7a4b3ff49c7cc276446193ae6fdf97c
tpsdevelopment2015 <tpsdevelopment2015@gmail.com>
2016-10-28 09:46:35 -0500

    Add files via upload

---
+++ CodeSample/CodeSample/CodeSample/JumpToConclusions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CodeSample
+{
+    class JumpToConclusionsBase
+    {
+        public int[] GameBoard { get; set; }
+
+        public JumpToConclusionsBase(int[] gameBoard)
+        {
+            GameBoard = gameBoard;
+        }
+
+        public virtual int CalculateCost()
+        {
+           
+            throw new NotImplementedException();
+        }
+    }
+}
