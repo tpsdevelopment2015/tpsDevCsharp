b12e3cffac606300369f3cab3909590c3d65a07a
tpsdevelopment2015 <tpsdevelopment2015@gmail.com>
2017-01-11 17:03:38 -0600

    added class Account

---
+++ AccountManagerApp/AccountManagerApp/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountManagerApp
+{
+    class Account
+    {
+        public string acctName { get; set; }
+        public string acctNumber { get; set; }
+        public string acctUserName { get; set; }
+        public string acctPassword { get; set; }
+        public string acctNotes { get; set; }
+
+        public Account(string name, string number, string userName, string password, string notes)
+        {
+            acctName = name;
+            acctNumber = number;
+            acctUserName = userName;
+            acctPassword = password;
+            acctNotes = notes;
+
+        }//end constructor
+
+
+    }//end class
+}//end namespace
