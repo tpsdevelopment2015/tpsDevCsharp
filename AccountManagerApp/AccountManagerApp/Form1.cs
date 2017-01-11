bce974f00eeecd4776929e90978b3a24bee7b271
tpsdevelopment2015 <tpsdevelopment2015@gmail.com>
2017-01-11 14:16:20 -0600

    exit message box modified

--- AccountManagerApp/AccountManagerApp/Form1.cs
+++ AccountManagerApp/AccountManagerApp/Form1.cs
@@ -29,8 +29,8 @@ namespace AccountManagerApp
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("DO YOU WANT TO SAVE CHANGES?", "SAVE CHANGES",
-            MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            MessageBox.Show("CHANGES WILL BE SAVED.", "SAVE CHANGES",
+            MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
 
             Application.Exit();
         }
