6a85d175ef94b973250c0dbd2bbb265ce983fa43
tpsdevelopment2015 <tpsdevelopment2015@gmail.com>
2017-01-24 14:57:55 -0600

    added Chrome Killer app

---
+++ ChromeKiller/ChromeKiller/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChromeKiller
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+
+        }//end Form1()
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (System.Diagnostics.Process proc in System.Diagnostics.Process.GetProcessesByName("chrome"))
+                {
+                    proc.Kill();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            Application.Exit();
+
+        }//end Click()
+
+        
+    }//end class
+}// end namespace
