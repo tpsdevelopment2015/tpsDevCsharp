ffc849b16bc69010f2d93b804cfd2fd2f4ecea5c
tpaulson <tpsDevelopment2015@gmail.com>
2017-01-31 10:58:11 -0600

    intial Commit MathGameV2 refactored with NUnit Unit Testing

---
+++ MathGameV2/MathGameV2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGameV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+        }//end Main()
+
+
+
+
+    }//end class
+}//end namespace
