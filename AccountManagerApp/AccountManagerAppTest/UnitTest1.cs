d182ed42838ce4e6af49c69aef66025ca5e84362
tpaulson <tpsDevelopment2015@gmail.com>
2017-02-08 17:36:45 -0600

    Added unit test project

---
+++ AccountManagerApp/AccountManagerAppTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AccountManagerAppTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
