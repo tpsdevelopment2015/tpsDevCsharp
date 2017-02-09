26e59d103257f7841a75fb2d99e5d5937141d684
tpaulson <tpsDevelopment2015@gmail.com>
2017-02-09 08:56:30 -0600

    Started adding test methods to follow TDD

--- AMEEXPCalc/AMEEXPCalc/MainWindow.xaml.cs
+++ AMEEXPCalc/AMEEXPCalc/MainWindow.xaml.cs
@@ -23,6 +23,10 @@ namespace AMEEXPCalc
         public MainWindow()
         {
             InitializeComponent();
+            
+
         }
+        
+       
     }
 }
---
+++ AMEEXPCalc/UnitTestProject1/MainWindowTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System.Collections;
+
+namespace AMEEXPCalcTest
+{
+    [TestFixture]
+    public class MainWindowTests
+    {
+        [Test]
+        public void MainWindow_CreatesDictionary_ReturnsDictonaryInstance()
+        {
+            
+
+        }
+
+
+
+    }//end class
+}//end namespace
